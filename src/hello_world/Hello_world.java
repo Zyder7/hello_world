a9ab5b329127c8d3588a9e7d3f55a410f2a7179d
BRAUN.JUSTIN <BRAUN.JUSTIN@berufskolleg-aachen.de>
2016-11-29 10:58:55 +0100

    first commit

---
+++ src/hello_world/Hello_world.java
@@ -0,0 +1,12 @@
+package hello_world;
+
+/**
+ * @author braun.Justin
+ */
+public class Hello_world {
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+    
+}
