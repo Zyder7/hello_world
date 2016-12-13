01a09bf3aca5af1d08d8d6e4cbd3234988480219
Zyder7 <braunJ777@gmail.com>
2016-12-13 10:44:22 +0100

    Englisch Übersetzung

--- src/hello_world/Hello_world.java
+++ src/hello_world/Hello_world.java
@@ -6,7 +6,7 @@ package hello_world;
 public class Hello_world {
 
     public static void main(String[] args) {
-        System.out.println("hello world");
+        System.out.println("hallo welt");
     }
     
 }
